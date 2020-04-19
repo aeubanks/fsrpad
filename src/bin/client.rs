bed71181d4762f34a1755921f33f0125d90e27ff
Arthur Eubanks <ayueubanks@gmail.com>
2020-04-18 21:59:08 -0700

    Write readings to a file in client
    Uses the Stepmania sextet stream format

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,5 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -7,11 +9,25 @@ struct Opt {
     #[structopt(short, long, parse(from_os_str))]
     file: PathBuf,
 
-    #[structopt(short, long)]
+    #[structopt(short, long, default_value = "raspberrypi:8000")]
     server: String,
 }
 
-fn main() {
+fn sextext_from_reading(reading: u8) -> u8 {
+    ((reading + 0x10) & 0x3F) + 0x30
+}
+
+fn main() -> std::io::Result<()> {
     let opts = Opt::from_args();
-    println!("{:?}", opts);
+    let mut stream = TcpStream::connect(opts.server).unwrap();
+    let mut fd = std::fs::OpenOptions::new()
+        .write(true)
+        .open(opts.file)
+        .unwrap();
+    loop {
+        stream.write(&[0]).unwrap();
+        let mut res = [0];
+        stream.read(&mut res).unwrap();
+        fd.write(&[sextext_from_reading(res[0]), 0x0A]).unwrap();
+    }
 }
