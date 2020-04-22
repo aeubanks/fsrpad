84a9b4f7d576f7a4ff85ffe4bb5122189046e3bf
Arthur Eubanks <ayueubanks@gmail.com>
2020-04-21 19:48:19 -0700

    Add client option to quit on network error

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -18,8 +18,11 @@ struct Opt {
     #[structopt(short, long)]
     wait_duration: u64,
 
-    #[structopt(short, long)]
-    quit_on_error: bool,
+    #[structopt(long)]
+    quit_on_file_error: bool,
+
+    #[structopt(long)]
+    quit_on_network_error: bool,
 }
 
 fn sextext_from_reading(reading: u8) -> u8 {
@@ -37,12 +40,18 @@ fn main() -> std::io::Result<()> {
     loop {
         std::thread::sleep(wait_duration);
         let start = std::time::Instant::now();
-        stream.write(&[0]).unwrap();
+        let ret = stream.write(&[0]);
+        if opts.quit_on_network_error {
+            let _ = ret.unwrap();
+        }
         let mut res = [0];
-        stream.read(&mut res).unwrap();
-        let write = fd.write(&[sextext_from_reading(res[0]), 0x0A]);
-        if opts.quit_on_error {
-            let _ = write.unwrap();
+        let ret2 = stream.read(&mut res);
+        if opts.quit_on_network_error {
+            let _ = ret2.unwrap();
+        }
+        let ret3 = fd.write(&[sextext_from_reading(res[0]), 0x0A]);
+        if opts.quit_on_file_error {
+            let _ = ret3.unwrap();
         }
         if opts.verbose {
             println!("{:07?} microseconds", start.elapsed().as_micros());
